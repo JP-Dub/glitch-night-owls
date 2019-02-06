439f48304d09a233f283eac2cdc712bbbf533789
Glitch (jp-dub-night-owls) <none>
2019-02-06 22:58:46 +0000

    💵😎 Checkpoint ./app/controllers/clickHandler.server.js:553280/118 ./public/index.html:553280/4839 ./app/controllers/clickController.client.js:553280/64 ./app/controllers/userController.client.js:553280/62

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -1,33 +1,33 @@
 'use strict';
 /* global ajax appUrl */
-(function () {
+// (function () {
 
-   var addButton = document.querySelector('.btn-add');
-   var deleteButton = document.querySelector('.btn-delete');
-   var clickNbr = document.querySelector('#click-nbr');
-   var apiUrl = appUrl + '/api/:id/clicks';
+//    var addButton = document.querySelector('.btn-add');
+//    var deleteButton = document.querySelector('.btn-delete');
+//    var clickNbr = document.querySelector('#click-nbr');
+//    var apiUrl = appUrl + '/api/:id/clicks';
 
-   function updateClickCount (data) {
-      var clicksObject = JSON.parse(data);
-      clickNbr.innerHTML = clicksObject.clicks;
-   }
+//    function updateClickCount (data) {
+//       var clicksObject = JSON.parse(data);
+//       clickNbr.innerHTML = clicksObject.clicks;
+//    }
 
-   ajax.Ready(ajax.Request('GET', apiUrl, updateClickCount));
+//    ajax.Ready(ajax.Request('GET', apiUrl, updateClickCount));
 
-   addButton.addEventListener('click', function () {
+//    addButton.addEventListener('click', function () {
 
-      ajax.Request('POST', apiUrl, function () {
-         ajax.Request('GET', apiUrl, updateClickCount);
-      });
+//       ajax.Request('POST', apiUrl, function () {
+//          ajax.Request('GET', apiUrl, updateClickCount);
+//       });
 
-   }, false);
+//    }, false);
 
-   deleteButton.addEventListener('click', function () {
+//    deleteButton.addEventListener('click', function () {
 
-      ajax.Request('DELETE', apiUrl, function () {
-         ajax.Request('GET', apiUrl, updateClickCount);
-      });
+//       ajax.Request('DELETE', apiUrl, function () {
+//          ajax.Request('GET', apiUrl, updateClickCount);
+//       });
 
-   }, false);
+//    }, false);
 
-})();
+// })();
--- app/controllers/userController.client.js
+++ app/controllers/userController.client.js
@@ -35,14 +35,13 @@
             //event.preventDefault();
             if(!userId) return alert('You have to be logged in to perform this action!');
             let name = (this.parentNode.parentNode.id).slice(13);// id (number) of businesscard
-            //console.log('name', this)
+            
             let logBars = {
                 userId : userId,
                 name      : bars[name].name,
                 id        : bars[name].id
                 };
             
-            //console.log(logBars)
             $.post('api/:id/clicks', logBars, function(bar) {
                console.log('post(api/clicks)', bar)
               let going = document.getElementById(bar.id);
