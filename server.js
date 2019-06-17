0956872c49e9d2e4cd37af08c6b88d31fec6eb31
Glitch (jp-dub-night-owls) <none>
2019-06-17 21:13:27 +0000

    😬🌷 Checkpoint ./package.json:553280/9221 ./server.js:553280/14

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var express = require('express'),
 
 app.options('/', cors());    
  
-require('dotenv').load();
+require('dotenv').config();
 require('./app/config/passport')(passport);
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser : true});
