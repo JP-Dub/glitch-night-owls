471dbd45b2a52ef700fafa2decb6349339bfa1fb
Glitch (jp-dub-night-owls) <none>
2019-06-27 18:40:54 +0000

    🌱💂 Checkpoint ./package.json:553280/2977 ./server.js:553280/8 ./app/routes/index.js:553280/18

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use(passport.session());
 
 routes(app, passport, cors);
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
