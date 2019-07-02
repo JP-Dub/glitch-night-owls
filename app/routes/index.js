47d8631f7cc92a74e2f6903386bb5b5a87d1d1ee
Glitch (jp-dub-night-owls) <none>
2019-07-02 14:03:47 +0000

    🛩💝 Checkpoint ./app/routes/index.js:553280/60

--- app/routes/index.js
+++ app/routes/index.js
@@ -20,27 +20,27 @@ module.exports = (app, passport, cors) => {
 // 			res.sendFile(path + '/dist/index.html');
 // 		});
 	
-	app.route('/login/:user')
+	app.route( '/login/:user' )
 		.get(isLoggedIn, (req, res) => {
-			res.sendFile(process.cwd() + '/dist/index.html');
+			res.sendFile( process.cwd() + '/dist/index.html' );
     //res.json({success: '/login'})
 		});
 		
-	app.route('/user/:location')	
-		.get(handleServer.userLocation);
+	app.route( '/user/:location' )	
+		.get( handleServer.userLocation );
 			
-	app.route('/businesses/:search')
-		.post(handleServer.getNightlife);
+	app.route( '/businesses/:search' )
+		.post( handleServer.getNightlife );
 	
-	app.route('/api/:id/clicks')
-		.get(handleServer.getClicks)
-		.post(handleServer.addClick);		
+	app.route( '/api/:id/clicks' )
+		.get(  handleServer.getClicks )
+		.post( handleServer.addClick );		
 		
-	app.get('/auth/twitter', passport.authenticate('twitter'));
+	app.get( '/auth/twitter', passport.authenticate( 'twitter' ) );
 
-	app.route('/auth/twitter/callback')
-		.get(passport.authenticate('twitter', 
-    { failureRedirect: '/' }), (req, res) => {
+	app.route( '/auth/twitter/callback' )
+		.get( passport.authenticate( 'twitter', {failureRedirect: '/'} ), 
+        (req, res) => {
           let user = req.user.twitter['username'];
     	    res.redirect('/login/' + user);
 		});	
