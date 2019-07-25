43d321d698d0548e90d04e1842086466a59b5b05
Glitch (jp-dub-night-owls) <none>
2019-07-25 14:49:02 +0000

    🔔🎓 Checkpoint ./app/controllers/userController.client.js:553280/207 ./public/css/style.css:553280/821

--- app/controllers/userController.client.js
+++ app/controllers/userController.client.js
@@ -94,202 +94,202 @@ document.addEventListener("DOMContentLoaded", () => {
       };
     };
     
-     const createMainDiv = (obj) => {   
-       let length = obj.length,
-           dist   = obj[length-1].distance,
-           zip;
+    const createMainDiv = (obj) => {   
+      let length = obj.length,
+          dist   = obj[length-1].distance,
+          zip;
        
-       for(var i = 0; i < length; i++) {
-         let div          = document.createElement("DIV"),
-             img_div      = document.createElement('DIV'),
-             business_div = document.createElement('DIV'),
-             h2_ele       = document.createElement('H2'),
-             p_ele        = document.createElement('P'),
-             costDescription;
-               
-         main.appendChild(div);
-         div.id                 = 'businesscard_' + i;
-         div.className          = 'container'; 
-         img_div.className      = 'img-holder';
-         business_div.className = 'business';
+      for(var i = 0; i < length; i++) {
+        let div          = document.createElement("DIV"),
+            img_div      = document.createElement('DIV'),
+            business_div = document.createElement('DIV'),
+            h2_ele       = document.createElement('H2'),
+            p_ele        = document.createElement('P'),
+            costDescription;
+              
+        main.appendChild(div);
+        div.id                 = 'businesscard_' + i;
+        div.className          = 'container'; 
+        img_div.className      = 'img-holder';
+        business_div.className = 'business';
 
-         let businesscard = document.getElementById(div.id);
-         businesscard.appendChild(img_div);
-         businesscard.appendChild(business_div);     
+        let businesscard = document.getElementById(div.id);
+        businesscard.appendChild(img_div);
+        businesscard.appendChild(business_div);     
 
-         if(!obj[i].price) {
-           obj[i].price = '';         
-         }
+        if(!obj[i].price) {
+          obj[i].price = '';         
+        }
 
-         costDescription = {
-           '$'   : 'Inexpensive',
-           '$$'  : 'Moderate',
-           '$$$' : 'Pricey',
-           '$$$$': 'Ultra High End',
-           ''    : 'Unavailable'
-         }         
+        costDescription = {
+          '$'   : 'Inexpensive',
+          '$$'  : 'Moderate',
+          '$$$' : 'Pricey',
+          '$$$$': 'Ultra High End',
+          ''    : 'Unavailable'
+        }         
 
-         // nightlife cache
-         let identity = {
-           "id"  : obj[i].id,
-           "name": obj[i].name
-         };
+        // nightlife cache
+        let identity = {
+          "id"  : obj[i].id,
+          "name": obj[i].name
+        };
 
-         bars.push(identity);       
+        bars.push(identity);       
 
-         // if statement used when getLocation() is called prior to loading the screen
-         if(typeof locale === "object" && locale != null) {        
-           obj[i].alias = obj[i].alias + '?start=' + locale.latitude + '%20' + locale.longitude;
-         }
+        // if statement used when getLocation() is called prior to loading the screen
+        if(typeof locale === "object" && locale != null) {        
+          obj[i].alias = obj[i].alias + '?start=' + locale.latitude + '%20' + locale.longitude;
+        }
          
-         // find closest zip code to coordinates
-         if(dist > obj[i].distance) {
-           dist = obj[i].distance;
-           zip  = obj[i].location.zip_code;
-         }
+        // find closest zip code to coordinates
+        if(dist > obj[i].distance) {
+          dist = obj[i].distance;
+          zip  = obj[i].location.zip_code;
+        }
          
-         // write value of zip code to search bar
-         if(i === length -1) {
-           if(!input.value) input.placeholder = zip;
-         }  
+        // write value of zip code to search bar
+        if(i === length -1) {
+          if(!input.value) input.placeholder = zip;
+        }  
 
-         // no image will revert to 'no image available' icon
-         if(!obj[i].image_url) obj[i].image_url = '../public/img/NoProductImage_300.jpg';            
+        // no image will revert to 'no image available' icon
+        if(!obj[i].image_url) obj[i].image_url = '../public/img/NoProductImage_300.jpg';            
 
-         img_div.appendChild(document.createElement('IMG'));
-         img_div.firstChild.className = 'img-thumbnail';
-         img_div.firstChild.setAttribute('alt', 'image-url');
-         img_div.firstChild.setAttribute('src', obj[i].image_url);
-         img_div.appendChild(document.createElement('BR'));
-         img_div.appendChild(document.createElement('BUTTON'));
-         img_div.lastChild.className = "bttn";
-         img_div.lastChild.setAttribute('title', 'Let people know that you are going by pushing the button');
-         img_div.lastChild.setAttribute('type', 'button');
-         img_div.lastChild.setAttribute('value', 'submit');
-         img_div.lastChild.innerHTML = "Going ";
-         img_div.lastChild.appendChild(document.createElement('SPAN'));
-         img_div.childNodes[2].lastChild.setAttribute('id', obj[i].id )
-         img_div.childNodes[2].lastChild.classList.add('badge');
-         img_div.childNodes[2].lastChild.innerHTML = 0;
+        img_div.appendChild(document.createElement('IMG'));
+        img_div.firstChild.className = 'img-thumbnail';
+        img_div.firstChild.setAttribute('alt', 'image-url');
+        img_div.firstChild.setAttribute('src', obj[i].image_url);
+        img_div.appendChild(document.createElement('BR'));
+        img_div.appendChild(document.createElement('BUTTON'));
+        img_div.lastChild.className = "bttn";
+        img_div.lastChild.setAttribute('title', 'Let people know that you are going by pushing the button');
+        img_div.lastChild.setAttribute('type', 'button');
+        img_div.lastChild.setAttribute('value', 'submit');
+        img_div.lastChild.innerHTML = "Going ";
+        img_div.lastChild.appendChild(document.createElement('SPAN'));
+        img_div.childNodes[2].lastChild.setAttribute('id', obj[i].id )
+        img_div.childNodes[2].lastChild.classList.add('badge');
+        img_div.childNodes[2].lastChild.innerHTML = 0;
 
-         business_div.appendChild(h2_ele).setAttribute('title', 'Visit Website');
-         h2_ele.appendChild(document.createElement('A')).setAttribute('href', obj[i].url)
-         h2_ele.firstChild.innerHTML = obj[i].name;
+        business_div.appendChild(h2_ele).setAttribute('title', 'Visit Website');
+        h2_ele.appendChild(document.createElement('A')).setAttribute('href', obj[i].url)
+        h2_ele.firstChild.innerHTML = obj[i].name;
 
-         business_div.appendChild(document.createElement('BR'));
-         business_div.appendChild(p_ele).className = 'address';
+        business_div.appendChild(document.createElement('BR'));
+        business_div.appendChild(p_ele).className = 'address';
 
-         p_ele.appendChild(document.createElement('A')).setAttribute('href', "https://www.yelp.com/map/" + obj[i].alias);
-         p_ele.firstChild.innerHTML = obj[i].location.address1 + `<br>` 
-                                      + obj[i].location.city + `, ` 
-                                      + obj[i].location.state + `. ` 
-                                      + obj[i].location.zip_code;
+        p_ele.appendChild(document.createElement('A')).setAttribute('href', "https://www.yelp.com/map/" + obj[i].alias);
+        p_ele.firstChild.innerHTML = obj[i].location.address1 + `<br>` 
+                                     + obj[i].location.city + `, ` 
+                                     + obj[i].location.state + `. ` 
+                                     + obj[i].location.zip_code;
 
-         p_ele.appendChild(document.createElement('BR'));
-         p_ele.appendChild(document.createElement('SPAN'));
-         p_ele.childNodes[2].classList.add('phone');
-         p_ele.childNodes[2].innerHTML ='Telephone:';
-         p_ele.childNodes[2].setAttribute('href', obj[i].phone);
-         p_ele.childNodes[2].setAttribute('title', 'Call Number');
-         p_ele.childNodes[2].appendChild(document.createElement('A')).innerHTML = obj[i].display_phone;
+        p_ele.appendChild(document.createElement('BR'));
+        p_ele.appendChild(document.createElement('SPAN'));
+        p_ele.childNodes[2].classList.add('phone');
+        p_ele.childNodes[2].innerHTML ='Telephone:';
+        p_ele.childNodes[2].setAttribute('href', obj[i].phone);
+        p_ele.childNodes[2].setAttribute('title', 'Call Number');
+        p_ele.childNodes[2].appendChild(document.createElement('A')).innerHTML = obj[i].display_phone;
 
-         p_ele.appendChild(document.createElement('BR'));
-         p_ele.appendChild(document.createElement('SPAN')).classList.add('rate');
-         p_ele.childNodes[4].innerHTML = "Price: " + obj[i].price + " " + costDescription[obj[i].price];
+        p_ele.appendChild(document.createElement('BR'));
+        p_ele.appendChild(document.createElement('SPAN')).classList.add('rate');
+        p_ele.childNodes[4].innerHTML = "Price: " + obj[i].price + " " + costDescription[obj[i].price];
  
-         p_ele.appendChild(document.createElement('BR'));
-         p_ele.appendChild(document.createElement('SPAN'));
-         p_ele.childNodes[6].innerHTML = 'Rating: ' + obj[i].rating;         
+        p_ele.appendChild(document.createElement('BR'));
+        p_ele.appendChild(document.createElement('SPAN'));
+        p_ele.childNodes[6].innerHTML = 'Rating: ' + obj[i].rating;         
 
-       }; // for(loop)
+      }; // for(loop)
         
-       loadBttnEvents();
-     };   
+      loadBttnEvents();
+    };   
 
-     let url = '/businesses/search?term=bars&location=';        
-     url += typeof locale === 'object' ? locale.latitude + '%20' + locale.longitude 
+    let url = '/businesses/search?term=bars&location=';        
+    url += typeof locale === 'object' ? locale.latitude + '%20' + locale.longitude 
                                         : locale;
       
-     // verify data to be sent;
-     let data = !userId ? {} : {user: userId};
+    // verify data to be sent;
+    let data = !userId ? {} : {user: userId};
 
-     ajax.ready(ajax.request("POST", url, data, (res) => {
-       let obj = JSON.parse(res);
-       if(obj.error) return alert(res);
-         createMainDiv(obj);
-     }));
-   }; // postResults()
+    ajax.ready(ajax.request("POST", url, data, (res) => {
+      let obj = JSON.parse(res);
+      if(obj.error) return alert(res);
+        createMainDiv(obj);
+    }));
+  }; // postResults()
   
      
-   // listener for Twitter login button
-   twitter.addEventListener("click", (evt) => {
+  // listener for Twitter login button
+  twitter.addEventListener("click", (evt) => {
+    evt.preventDefault();
+     
+    window.location.href = '/auth/twitter';
+  });
+  
+  // listener for Search button
+  search.addEventListener("click", (evt) => {
      evt.preventDefault();
      
-     window.location.href = '/auth/twitter';
-   });
-  
-   // listener for Search button
-   search.addEventListener("click", (evt) => {
-      evt.preventDefault();
-     
-      let location = document.getElementById("location").elements[1].value;
-      if(bars.length) bars = [];     
-      !location? getLocation() : postResults(location);
-   });  
+     let location = document.getElementById("location").elements[1].value;
+     if(bars.length) bars = [];     
+     !location? getLocation() : postResults(location);
+  });  
   
 
-   // checks if user is logged in /  returns previous session
-   if( loggedIn ) {     
-     ajax.ready(ajax.request('GET', '/user/location', {}, (req) => {
-        let user     = req.twitter,
-            location = user.previousSession;
-            if(!location) {
-              location = req.session.location
-            }
+  // checks if user is logged in /  returns previous session
+  if( loggedIn ) {     
+    ajax.ready(ajax.request('GET', '/user/location', {}, (req) => {
+       let user     = req.twitter,
+           location = user.previousSession;
+           if(!location) {
+             location = req.session.location
+           }
        
-        userId = user.id;
-           
-        //console.log('req', req)
-        input.setAttribute('placeholder', location || user.location);
+       userId = user.id;
+          
+       //console.log('req', req)
+       input.setAttribute('placeholder', location || user.location);
        
-        return postResults(location);
-     }));
-   };
+       return postResults(location);
+    }));
+  };
      
-   // run if search bar is empty when a search is exec.
-   function getLocation(done) {
-      if (navigator.geolocation) {
-         navigator.geolocation.getCurrentPosition(function(position) {
+  // run if search bar is empty when a search is exec.
+  function getLocation(done) {
+     if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
            
-           postResults({
-             latitude : position.coords.latitude,
-             longitude: position.coords.longitude
-           });
+          postResults({
+            latitude : position.coords.latitude,
+            longitude: position.coords.longitude
+          });
            
-         }, showError);
-      } else {
-         console.log("Geolocation is not supported by this browser.");
-      };
-   };
+        }, showError);
+     } else {
+        console.log("Geolocation is not supported by this browser.");
+     };
+  };
   
-    // used in conjunction with getLocation()  
-   function showError(error) {
-      switch(error.code) {
-         case error.PERMISSION_DENIED:
-            console.log("User denied the request for Geolocation.");
-            break;
-         case error.POSITION_UNAVAILABLE:
-            console.log("Location information is unavailable.");
-            break;
-         case error.TIMEOUT:
-            console.log("The request to get user location timed out.");
-            break;
-         case error.UNKNOWN_ERROR:
-            console.log("An unknown error occurred.");
-            break;
-      };
-   };
-   //getLocation()
+   // used in conjunction with getLocation()  
+  function showError(error) {
+     switch(error.code) {
+        case error.PERMISSION_DENIED:
+           console.log("User denied the request for Geolocation.");
+           break;
+        case error.POSITION_UNAVAILABLE:
+           console.log("Location information is unavailable.");
+           break;
+        case error.TIMEOUT:
+           console.log("The request to get user location timed out.");
+           break;
+        case error.UNKNOWN_ERROR:
+           console.log("An unknown error occurred.");
+           break;
+     };
+  };
+  
 });
 
 
