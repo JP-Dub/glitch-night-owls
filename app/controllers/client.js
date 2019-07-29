4092e45495001eaeb3a1d55c6a170c7beb9db33b
Glitch (jp-dub-night-owls) <none>
2019-07-29 22:07:53 +0000

    🍦🎳 Checkpoint ./app/controllers/client.js:553280/37

--- app/controllers/client.js
+++ app/controllers/client.js
@@ -53,9 +53,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
   
-  ajax.ready(ajax.request('DELETE', '/api/resetRSVP', (req) => {
-    console.log(req)
-  }));
+  // ajax.ready(ajax.request('DELETE', '/api/resetRSVP', {}, (req) => {
+  //   console.log(req)
+  // }));
      
    // load RSVP data to buttons and attach event listener
   function loadBttnEvents() { 
