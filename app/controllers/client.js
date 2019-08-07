9fcec9d3ac53c447f13646e8e82a457e1fc8a936
Glitch (jp-dub-night-owls) <none>
2019-08-07 14:51:48 +0000

    moved showError() inside of getLocation()

--- app/controllers/client.js
+++ app/controllers/client.js
@@ -257,25 +257,25 @@ document.addEventListener("DOMContentLoaded", () => {
      } else {
         alert("Geolocation is not supported by this browser.");
      };
+    
+      // used in conjunction with getLocation()  
+      function showError(error) {
+         switch(error.code) {
+            case error.PERMISSION_DENIED:
+               console.log("User denied the request for Geolocation.");
+               break;
+            case error.POSITION_UNAVAILABLE:
+               console.log("Location information is unavailable.");
+               break;
+            case error.TIMEOUT:
+               console.log("The request to get user location timed out.");
+               break;
+            case error.UNKNOWN_ERROR:
+               console.log("An unknown error occurred.");
+               break;
+         };
+      };   
   };
-  
-  // used in conjunction with getLocation()  
-  function showError(error) {
-     switch(error.code) {
-        case error.PERMISSION_DENIED:
-           console.log("User denied the request for Geolocation.");
-           break;
-        case error.POSITION_UNAVAILABLE:
-           console.log("Location information is unavailable.");
-           break;
-        case error.TIMEOUT:
-           console.log("The request to get user location timed out.");
-           break;
-        case error.UNKNOWN_ERROR:
-           console.log("An unknown error occurred.");
-           break;
-     };
-  };   
   
   // checks if user is logged in /  returns previous session
   if( loggedIn ) {     
